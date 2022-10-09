0a36b19892a46d9649b891fcd071ef3fd1a66012
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-08 23:16:10 -0400

    commit tree 6e52887ab67b4165ed62bb7f55c8008cdda8d5f4

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(int)
+   while int > 0 do
+      puts "#{int} SECOND(S)!"
+      int -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+    sleep int
+end 
